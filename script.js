01ca1bb5cb86568ec773b318f77b9711d7551efb
Benjamin San Souci <benjamin.sansouci@gmail.com>
2013-10-08 21:23:11 -0700

    code cleaning and timing can compare arrays of functions
    - compareTime function now can compare arrays of functions
    - moved the getRandomArray() function into a more appropriate place
    (inside the main script and not the timing method)

--- script.js
+++ script.js
@@ -4,7 +4,16 @@ $(document).ready(function () {
 	loadScript(["lib/timing.js", "Sorting/MergeSort.js", "Sorting/QuickSort.js"], function () {
 		// Timer can time your algorithms, see which one is more efficient
 		var timer = Timer();
-		var allData = timer.compareTime(quicksort, mergesort, getRandomArray, 2);
-		console.log("Mergesort data: '" + timer.averageOutArray(allData[1]) + "', Quicksort data: '" + timer.averageOutArray(allData[0]) + "'");
+		var allData = timer.compareTime([quicksort, mergesort], getRandomArray, 10);
+		console.log("50 tries of -> Mergesort data: " + timer.averageOutArray(allData[1]) + "ms, Quicksort data: " + timer.averageOutArray(allData[0]) + "ms");
 	});
 });
+
+function getRandomArray() {
+	var array = [];
+	var length = 100000;
+	for (var i = 0; i < length; i++) {
+		array[i] = Math.round(Math.random() * length);
+	}
+	return array;
+}
