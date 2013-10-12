89243c3290382450f6be27c8206949fefc3ccfec
Benjamin San Souci <benjamin.sansouci@gmail.com>
2013-10-11 17:23:21 -0700

    In-place quicksort finally works
    I don't know why it wasn't working before, but now in-place quicksort
    works. As you can see, it's about 10x faster than normal quicksort ->
    memory allocation takes a lot of time.

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 $(document).ready(function () {
 	// First load the sorting algorithms, then loader the timer which 
 	// will stress test them and time them
-	loadScript(["lib/timing.js", "Sorting/MergeSort.js", "Sorting/QuickSort.js", "Sorting/BubbleSort.js", "Sorting/QuickSortInPlace.js"], function () {
+	loadScript(["lib/timing.js", "Sorting/MergeSort.js", "Sorting/QuickSort.js", "Sorting/BubbleSort.js", "Sorting/InPlaceQuickSort.js"], function () {
 		// Timer can time your algorithms, see which one is more efficient
 		var timer = Timer();
 		var NUM_OF_TIMES = 1;
-		var allData = timer.compareTime([quicksort, mergesort, quicksortinplace], getRandomArray, 1);
+		var allData = timer.compareTime([quicksort, mergesort, inplacequicksort], getRandomArray, NUM_OF_TIMES);
 		console.log(NUM_OF_TIMES + " tries (with arrays of size 1000000) of -> Mergesort timing: " + timer.averageOutArray(allData[1]) + "ms, " +
 			"Quicksort timing: " + timer.averageOutArray(allData[0]) + "ms, " +
-			"Inplace QuickSort timing: " + timer.averageOutArray(allData[2]) + "ms"
-			);
+			"In-place QuickSort timing: " + timer.averageOutArray(allData[2]) + "ms"
+		);
 	});
 });
 
